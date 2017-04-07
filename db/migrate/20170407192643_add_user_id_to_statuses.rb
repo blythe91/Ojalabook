2d95b72d4f2618c926348a3aa16938114e63c2ae
Blythe91 <castro.oscar18@gmail.com>
2017-04-07 15:35:26 -0400

    Crear registro de Usuarios

--- db/migrate/20170407192643_add_user_id_to_statuses.rb
+++ db/migrate/20170407192643_add_user_id_to_statuses.rb
@@ -1,4 +1,8 @@
 class AddUserIdToStatuses < ActiveRecord::Migration[5.0]
   def change
+
+  	add_column :statuses, :usuario_id, :integer
+  	add_index :statuses, :usuario_id
+  	remove_column :statuses, :nombre
   end
 end
